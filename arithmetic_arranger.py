9fd0e1266fc9bc6553eae9fb7395c83353220eac
kkumykova <38696352+kkumykova@users.noreply.github.com>
2022-02-04 12:30:27 +0000

    add output strings description, handle first error

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,5 +1,16 @@
-def arithmetic_arranger(problems, result = False):
-    pass
+def arithmetic_arranger(problems, result=False):
 
-    # return arranged_string  # Return the arranged string.
+    # First Error Handling: Function must only accept a maximum of seven expressions
+    if len(problems) > 7:
+        return "Error: Too many problems."
 
+    # Output description:
+    # If result argument is True, there will be four output line, else - three lines.
+    line1 = ""  # the line containing the first operand
+    line2 = ""  # the line containing the operator and second operand
+    line3 = ""  # the line containing dashes
+    line4 = ""  # answer to each individual problem
+
+    arranged_string = ""
+
+    return arranged_string  # Return the arranged string.
