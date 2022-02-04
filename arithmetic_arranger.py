de07f8c8a882506adbbda3a6eec5d9e271a4bca8
kkumykova <38696352+kkumykova@users.noreply.github.com>
2022-02-04 19:40:18 +0000

    add line 4

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -10,48 +10,52 @@ def arithmetic_arranger(problem_list, result=False):
             problem = problem.split("+")
             if len(problem[0]) > 4 or len(problem[1]) > 4:
                 return "Error: Numbers cannot be more than four digits."
-            elif not(problem[0].isdigit() or problem[1].isdigit()):
+            elif not (problem[0].isdigit() or problem[1].isdigit()):
                 return "Error: Numbers must only contain digits."
 
         elif "-" in problem:
             problem = problem.split("-")
             if len(problem[0]) > 4 or len(problem[1]) > 4:
                 return "Error: Numbers cannot be more than four digits."
-            elif not(problem[0].isdigit() or problem[1].isdigit()):
+            elif not (problem[0].isdigit() or problem[1].isdigit()):
                 return "Error: Numbers must only contain digits."
 
         else:
             return "Error: Operator must be '+' or '-'."
 
-        if not result:
-            res = ''
-            line1 = ''
-            line2 = ''
-            line3 = ''
-            for i, sub in enumerate(problem_list):
-                first_operands = [''.join(sub.split()[0])]
-                operators = [''.join(sub.split()[1])]
-                second_operands = [''.join(sub.split()[2])]
+        arranged_string = ''
+        line1 = ''
+        line2 = ''
+        line3 = ''
+        line4 = ''
+        for i, sub in enumerate(problem_list):
+            first_operands = [''.join(sub.split()[0])]
+            operators = [''.join(sub.split()[1])]
+            second_operands = [''.join(sub.split()[2])]
 
-                for f in first_operands:
-                    for o in operators:
-                        for s in second_operands:
-                            # each operand not more than 4 digits second line should also have two more chars:
-                            # operator and a space b/w operator and sec operand in total 6 spaces should be reserved
-                            # after each operand, four spaces need to be added
-                            longest_digit = max(len(f), len(s)) + 2
-                            line1 += f.rjust(longest_digit) + "    "
-                            line2 += o + s.rjust(longest_digit-1) + "    "
-                            line3 += "-" * longest_digit + "    "
-            line1 = line1.rstrip()
-            line2 = line2.rstrip()  # Removing the last four whitespaces from each line
-            line3 = line3.rstrip()
-            res += "\n".join([line1, line2, line3])
+            for f in first_operands:
+                for o in operators:
+                    for s in second_operands:
+                        # each operand not more than 4 digits second line should also have two more chars:
+                        # operator and a space b/w operator and sec operand in total 6 spaces should be reserved
+                        # after each operand, four spaces need to be added
+                        longest_digit = max(len(f), len(s)) + 2
+                        line1 += f.rjust(longest_digit) + "    "
+                        line2 += o + s.rjust(longest_digit - 1) + "    "
+                        line3 += "-" * longest_digit + "    "
+                        operation_res = str(eval(f + o + s))
+                        line4 += operation_res.rjust(longest_digit) + "    "
 
-        return res.rjust(longest_digit) + "    "
+    line1 = line1.rstrip()
+    line2 = line2.rstrip()  # Removing the last four whitespaces from each line
+    line3 = line3.rstrip()
+    arranged_string += "\n".join([line1, line2, line3])
 
-# print(arithmetic_arranger(['3801 - 2', '123 + 49']))
-#
+    if result:  # Adding the fourth line if the result argument is True
+        line4 = line4.rstrip()
+        arranged_string += "\n" + line4 + "\n"
+
+    return arranged_string
 # print(arithmetic_arranger(['3801 - 2', '123 + 49']))
 
 
